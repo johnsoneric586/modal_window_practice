b7e97b7317132caa2cfe81cff5fb986bce7ac6ef
Eric Johnson <johnsoneric586@gmail.com>
2024-05-22 07:54:53 -0400

    Added hover effect to the 'x' element in the modal. Added overlay effect to the modal window.

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 'use strict';
 
 // ---------- Variables ----------
+// ----- Buttons -----
 const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// ----- DOM Elements -----
 const modal_el = document.querySelector('.modal');
+const overlay_el = document.querySelector('.overlay');
 
 // ---------- Functions ----------
 
 function revealModal() {
   modal_el.classList.remove('hidden');
+  overlay_el.classList.remove('hidden');
+}
+
+function hideModal() {
+  modal_el.classList.add('hidden');
+  overlay_el.classList.add('hidden');
 }
 
 // // ---------- Event Handlers ----------
 btnShowModal.forEach(function (button) {
   button.addEventListener('click', revealModal);
 });
+
+btnCloseModal.addEventListener('click', hideModal);

4bd5f65f4f866379a93550c2108a2d02ff8c44fa
Eric Johnson <johnsoneric586@gmail.com>
2024-05-22 07:46:18 -0400

    Created initial modal buttons variable and modal DOM element variable and added modal reveal functionality to the modal buttons.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// ---------- Variables ----------
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal_el = document.querySelector('.modal');
+
+// ---------- Functions ----------
+
+function revealModal() {
+  modal_el.classList.remove('hidden');
+}
+
+// // ---------- Event Handlers ----------
+btnShowModal.forEach(function (button) {
+  button.addEventListener('click', revealModal);
+});
